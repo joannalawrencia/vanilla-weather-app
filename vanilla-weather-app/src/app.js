ff43848fe85093480c5930f760a2a82b968a0e36
joannalawrencia <97447535+joannalawrencia@users.noreply.github.com>
2022-01-21 12:54:58 +0100

    created search engine with form and submit button, added Eventlistener

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -14,7 +14,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -41,8 +40,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let city = "Madrid";
+let city = "New York";
 
-let apiKey = "d15392c799af218cfbddce487fe22d57";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "d15392c799af218cfbddce487fe22d57";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
