a41bb23275c564fe24fbf093114ac74fe053a640
joannalawrencia <97447535+joannalawrencia@users.noreply.github.com>
2022-02-03 15:21:01 +0100

    implemented forecast API

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -21,9 +21,14 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
+function getForecast(coordinates) {
+  let apiKey = "d15392c799af218cfbddce487fe22d57";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+}
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
   days.forEach(function (day) {
@@ -76,6 +81,8 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
